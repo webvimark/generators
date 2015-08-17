bacea440be8983fe602db6b9789a466faac43b4f
vi mark <webvimark@gmail.com>
2015-08-17 21:00:56 +0200

    mm

--- crud/default/views/_form.php
+++ crud/default/views/_form.php
@@ -70,7 +70,7 @@ use webvimark\extensions\ckeditor\CKEditor;
 		</div>
 	<?php endif; ?>
 
-	<?= \$form->field(\$model, '$attribute', ['enableClientValidation'=>false, 'enableAjaxValidation'=>false])->fileInput(['class'=>'form-control']) ?>
+	<?= \$form->field(\$model, '$attribute', ['enableAjaxValidation'=>false])->fileInput(['class'=>'form-control']) ?>
 
 
 IMG;
